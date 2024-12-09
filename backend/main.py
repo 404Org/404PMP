fcf18ce364345139c53bacdcd7d6014b578ceda9
Prince-terawe <princek@terawe.com>
2024-12-09 16:29:59 +0530

    increase jwt expiration time

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from shared.database import init_db
-from shared.config import MONGO_URI, JWT_SECRET_KEY
+from shared.config import MONGO_URI, JWT_SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRES
 from auth.routes import auth
 from projects.routes import projects
+from datetime import timedelta
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,7 @@ CORS(app)
 # Configurations
 app.config["MONGO_URI"] = MONGO_URI
 app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = JWT_ACCESS_TOKEN_EXPIRES
 
 # Initialize database and JWT
 init_db(app)
--- backend/shared/config.py
+++ backend/shared/config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
@@ -5,3 +6,4 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
